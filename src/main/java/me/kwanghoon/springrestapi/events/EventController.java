686f320f7a13e9d6184d9cbfa90f371e0aed43b1
kwanghoon.kim <kk03187693@gmail.com>
2021-04-05 17:15:35 +0900

    add event controller example

---
+++ src/main/java/me/kwanghoon/springrestapi/events/EventController.java
@@ -0,0 +1,27 @@
+package me.kwanghoon.springrestapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(
+        @RequestBody Event event
+    ) {
+        URI uri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(uri).body(event);
+    }
+}
--- src/test/java/me/kwanghoon/springrestapi/events/EventControllerTests.java
+++ src/test/java/me/kwanghoon/springrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package me.kwanghoon.springrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(value = SpringExtension.class)
@@ -19,15 +24,33 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc; // 웹 서버를 띄우지 않지만 dispatcherServlet은 띄움
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+            .name("spring")
+            .description("rest api with spring")
+            .beginEnrollmentDateTime(LocalDateTime.of(2021, 4, 4, 4, 4))
+            .closeEnrollmentDateTime(LocalDateTime.of(2021, 5, 4, 4, 4))
+            .beginEventDateTime(LocalDateTime.of(2021, 6, 4, 4, 4))
+            .endEventDateTime(LocalDateTime.of(2021, 7, 4, 4, 4))
+            .basePrice(100)
+            .maxPrice(200)
+            .limitOfEnrollment(100)
+            .location("강남")
+            .build();
+
+
         mockMvc.perform(
             post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event))
             )
-            .andExpect(status().isCreated());
+            .andDo(print())  // 결과 값 출력
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists());
     }
-
-
 }

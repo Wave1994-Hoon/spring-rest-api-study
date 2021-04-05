88010c89024ff965b8915ad84772eb6940d45d99
kwanghoon.kim <kk03187693@gmail.com>
2021-04-05 16:43:39 +0900

    add controller test

---
+++ src/test/java/me/kwanghoon/springrestapi/events/EventControllerTests.java
@@ -0,0 +1,33 @@
+package me.kwanghoon.springrestapi.events;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(value = SpringExtension.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc; // 웹 서버를 띄우지 않지만 dispatcherServlet은 띄움
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(
+            post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+            )
+            .andExpect(status().isCreated());
+    }
+
+
+}
